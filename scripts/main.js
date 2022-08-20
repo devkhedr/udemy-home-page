000219cb65084cdc20d871a8aa1dd4c735ed818a
Mohamed Mahmoud <107374106+Mohammed-Khedr@users.noreply.github.com>
2022-08-20 07:38:29 +0200

    feat: search bar optimized, usable courses tabs

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,25 @@
-let pythonCourses = document.querySelector(".python-list");
+let courses = [
+  "Python",
+  "AWS",
+  "Data Science",
+  "Excel",
+  "Web Development",
+  "JavaScript",
+  "Drawing",
+];
+
+let cur_course = "Python";
+let section = document.querySelector(".courses");
+let data;
+function fetchdata() {
+  const response = fetch("https://api.npoint.io/f448aa5d80c56dda24a4")
+    .then((response) => response.json())
+    .then((res) => {
+      data = res;
+      //build the courses of the defult topic "python"
+      build_description(cur_course);
+    });
+}
 
 let getStars = function (courseRate) {
   let Res = [];
@@ -14,49 +35,129 @@ let getStars = function (courseRate) {
   return Res.join("\n");
 };
 
-let addCourseToList = function (course) {
-  const newCourse = document.createElement("div");
-  newCourse.classList.add("recommend");
-  newCourse.innerHTML = `
-  <img src=${course.image} alt=${course.title}/>
-  <h5>${course.title}</h5>
-  <div class="authorName">${course.author}</div>
-  <div class="rate">
-      ${course.rate}
-      ${getStars(course.rate)}
-  <span class = "number"> (${course.people})</span>
-  </div>
-  <span class = "price">E&pound;${course.price}</span>
+function build_description(course) {
+  section.innerHTML = "";
+  const description = `
+      <h1>${data.record[course].header}</h1>
+      <p>${data.record[course].description}</p>
+      <button class="explore-button"> Explore ${course} </button> 
   `;
-  pythonCourses.appendChild(newCourse);
-};
+  section.innerHTML += description;
+  make_cards_list(course);
+}
 
-
-
-let coursesList;
-
-async function fetchData() {
-  const response = await fetch("https://api.npoint.io/719889b51085a7230399");
-  coursesList = await response.json();
-  for (let i = 0; i < coursesList.length; i++) {
-    addCourseToList(coursesList[i]);
+function searchState (found, search) {
+  const description1 = `
+  <b style = "font-size:25px;margin-left:20px;"> 
+  Search results for "${search}" : <b><br><br>
+  `;
+  const description2 = `
+    <b style = "font-size:25px;margin-left:20px;"> Sorry, 
+    we couldn't find any results for "${search}" </b>
+    <br><br>
+    <b style = "font-size:18px;margin-left:20px;">
+    Try adjusting your search. Here are some ideas:</b>
+    <br><br>
+    <ul>
+      <li>Make sure all words are spelled correctly</li>
+      <li>Try different search terms</li>
+      <li>Try more general search terms</li>
+    </ul>
+  `;
+  if(found) {
+    return description1; 
+  }
+  else {
+    return description2;
   }
 }
-fetchData();
+
+function make_cards_list(course = "", search = undefined) {
+  let cards_container = document.createElement("div");
+  cards_container.className = "courses-list";
+  cards_container.id = "courses-list";
+  let found = 0;
+  for (const item of courses) {
+    if (search != undefined) course = item;
+    if (item != course) continue;
+
+    for (const item in data.record[course].courses) {
+      if (search != undefined) {
+        let title = data.record[course].courses[item].title;
+        title = title.toLowerCase();
+        search = search.toLowerCase();
+        if (!title.includes(search)) continue;
+      }
+      found = 1;
+      let ready_card = make_card(course, item);
+      cards_container.appendChild(ready_card);
+    }
+  }
+  const ret = searchState(found, search);
+  if(search == undefined) {
+    section.appendChild(cards_container);
+  }
+  else if((search != undefined) && found) {
+    section.innerHTML += ret;
+    section.appendChild(cards_container);
+  }
+  else if((search != undefined) && !found) {
+    section.innerHTML += ret;
+  }
+  
+}
+
+function make_card(course, item) {
+  const newCourse = document.createElement("div");
+  newCourse.classList.add("recommend");
+  let instructor = "";
+  for (const instr in data.record[course].courses[item].instructors) {
+    instructor +=
+      data.record[course].courses[item].instructors[instr].name + ", ";
+  }
+  instructor = instructor.slice(0, -2);
+  newCourse.innerHTML = `
+  <img src=${data.record[course].courses[item].image} alt=${
+    data.record[course].courses[item].title
+  }/>
+  <h5>${data.record[course].courses[item].title}</h5>
+  <div class="authorName">${instructor}</div>
+  <div class="rate">
+      ${data.record[course].courses[item].rating.toFixed(1)}
+      ${getStars(data.record[course].courses[item].rating.toFixed(1))}
+  </div>
+  <span class = "price">E&pound;${
+    data.record[course].courses[item].price
+  }</span>
+  `;
+  return newCourse;
+}
+
+fetchdata();
+
+for (const item in courses) {
+  const course = document.getElementById(courses[item]);
+  course.addEventListener("change", function () {
+    cur_course = course.id;
+    build_description(cur_course);
+  });
+}
 
 // search bar usable (search in client side)
+// building searching description
+function build_search(searchvalue) {
+  section.innerHTML = "";
+  make_cards_list("", searchvalue);
+}
 
-document.querySelector(".search-icon").addEventListener("click", (event) => {
-  event.preventDefault();
-});
-
-document.querySelector(".search-text").addEventListener("input", (event) => {
-  let List = document.querySelector(".python-list");
-  List.innerHTML = "";
-  for (let i = 0; i < coursesList.length; i++) {
-    let curTitle = coursesList[i].title.toLowerCase();
-    let hint = event.target.value.toLowerCase();
-    if (curTitle.includes(hint) === false) continue;
-    addCourseToList(coursesList[i]);
+// search on submit in search bar
+document.forms[0].onsubmit = function (e) {
+  e.preventDefault();
+  let searchvalue = document.querySelector("[class = 'search-text']").value;
+  let x = searchvalue.split(" ").length - 1;
+  document.getElementById("courses").scrollIntoView({behavior: "smooth", block: "start"});
+  // prevent searching on empty or only spaces values
+  if (searchvalue !== "" && x !== searchvalue.length) {
+    build_search(searchvalue);
   }
-});
+};
