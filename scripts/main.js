213a74b30759b2466907e6a90b553304d5867f22
Mohamed Mahmoud <107374106+Mohammed-Khedr@users.noreply.github.com>
2022-08-21 19:58:23 +0200

    feat: courses slider added

--- scripts/main.js
+++ scripts/main.js
@@ -8,16 +8,17 @@ let courses = [
   "Drawing",
 ];
 
-let cur_course = "Python";
-let section = document.querySelector(".courses");
+let curCourse = "Python";
+let courseSection = document.querySelector(".courses");
 let data;
-function fetchdata() {
+
+function fetchData() {
   const response = fetch("https://api.jsonbin.io/v3/b/63007c245c146d63ca771db2")
     .then((response) => response.json())
     .then((res) => {
       data = res;
       //build the courses of the defult topic "python"
-      build_description(cur_course);
+      buildCoursecourseSection(curCourse);
     });
 }
 
@@ -35,8 +36,8 @@ let getStars = function (courseRate) {
   return Res.join("\n");
 };
 
-function build_description(course) {
-  section.innerHTML = "";
+function buildCoursecourseSection(course) {
+  courseSection.innerHTML = "";
   const description = `
     <div class = "course-intro">
       <h1>${data.record[course].header}</h1>
@@ -44,11 +45,11 @@ function build_description(course) {
       <button class="explore-button"> Explore ${course} </button> 
     </div>
     `;
-  section.innerHTML += description;
-  make_cards_list(course);
+  courseSection.innerHTML += description;
+  makeCourseCards(course);
 }
 
-function searchState (found, search) {
+function searchState(found, search) {
   const description1 = `
   <b style = "font-size:25px;margin-left:20px;"> 
   Search results for "${search}" : <b><br><br>
@@ -66,43 +67,108 @@ function searchState (found, search) {
       <li>Try more general search terms</li>
     </ul>
   `;
-  if(found) {
-    return description1; 
-  }
-  else {
+  if (found) {
+    return description1;
+  } else {
     return description2;
   }
 }
 
-function make_cards_list(course = "", search = undefined) {
-  let cards_container = document.createElement("div");
-  cards_container.className = "courses-list";
-  cards_container.id = "courses-list";
-  let found = 0;
-  for (const item of courses) {
-    if (search != undefined) course = item;
-    if (item != course) continue;
+let n = 0;
 
-    for (const item in data.record[course].courses) {
-      if (search != undefined) {
-        let title = data.record[course].courses[item].title;
-        title = title.toLowerCase();
-        search = search.toLowerCase();
-        if (!title.includes(search)) continue;
-      }
-      found = 1;
-      let ready_card = make_card(course, item);
-      cards_container.appendChild(ready_card);
-    }
+function checkOnWindowwSize() {
+  if (window.matchMedia("(width <= 520px)").matches) {
+    n = 1;
+  } else if (window.matchMedia("(width <= 768px)").matches) {
+    n = 2;
+  } else if (window.matchMedia("(width <= 1015px)").matches) {
+    n = 3;
+  } else if (window.matchMedia("(width >= 1350px)").matches) {
+    n = 5;
+  } else {
+    n = 4;
   }
-  const ret = searchState(found, search);
-  if(search != undefined && search != "") {
-    section.innerHTML += ret;
-  }
-  section.appendChild(cards_container);
+
 }
 
-function make_card(course, item) {
+checkOnWindowwSize();
+
+window.addEventListener('resize', function () {
+  checkOnWindowwSize();
+  buildCoursecourseSection(curCourse);
+})
+
+function makeCourseCards(course = "", search = undefined) {
+  let cardsContainer = document.createElement("div");
+  cardsContainer.className = "carousel slide";
+  cardsContainer.id = "carouselExampleIndicators";
+  cardsContainer.setAttribute("data-ride", "carousel");
+  cardsContainer.setAttribute("position", "relative");
+  let allSlides = document.createElement("div");
+  allSlides.className = "carousel-inner";
+  let slideContainer = document.createElement("div");
+  slideContainer.className = "carousel-item active";
+  let slide = document.createElement("div");
+  slide.className = "courses-list";
+  let cnt = 0,
+    found = 0;
+  for (const item of courses) {
+    if (search != undefined) course = item;
+    if (item == course) {
+      for (const item in data.record[course].courses) {
+        if (search != undefined) {
+          let title = data.record[course].courses[item].title;
+          title = title.toLowerCase();
+          search = search.toLowerCase();
+          if (!title.includes(search)) continue;
+        }
+
+        if (cnt == n) {
+          slideContainer.appendChild(slide);
+          allSlides.appendChild(slideContainer);
+          slideContainer = document.createElement("div");
+          slideContainer.className = "carousel-item";
+          slide = document.createElement("div");
+          slide.className = "courses-list";
+          cnt = 0;
+        }
+        found = 1;
+        cnt++;
+        let cardlink = makeCard(course, item);
+        slide.appendChild(cardlink);
+      }
+    }
+  }
+  if (cnt != 0) {
+    slideContainer.appendChild(slide);
+    allSlides.appendChild(slideContainer);
+  }
+  cardsContainer.appendChild(allSlides);
+
+  const leftButton = `
+  <button class="carousel-control-prevv" href="#carouselExampleIndicators" type="button" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+`;
+  const rightButton = `
+  <button class="carousel-control-nextt" href="#carouselExampleIndicators" type="button" data-slide="next">
+    <span class=" carousel-control-next-icon"></span> 
+  </button>
+ `;
+  cardsContainer.innerHTML += leftButton;
+  cardsContainer.innerHTML += rightButton;
+
+  const ret = searchState(found, search);
+  if (search != undefined && search != "") {
+    courseSection.innerHTML += (ret);
+  }
+  if (found) {
+    courseSection.appendChild(cardsContainer);
+  }
+
+}
+
+function makeCard(course, item) {
   const newCourse = document.createElement("div");
   newCourse.classList.add("recommend");
   let instructor = "";
@@ -127,30 +193,32 @@ function make_card(course, item) {
   `;
   return newCourse;
 }
+fetchData();
 
-fetchdata();
-
-// move between tabs
+// moving between tabs
 for (const item in courses) {
   const course = document.getElementById(courses[item]);
   course.addEventListener("change", function () {
-    cur_course = course.id;
-    build_description(cur_course);
+    curCourse = course.id;
+    buildCoursecourseSection(curCourse);
   });
 }
 
 // search bar usable (search in client side)
-// building searching description
-function build_search(searchvalue) {
-  section.innerHTML = "";
-  make_cards_list("", searchvalue);
+function buildSearch(searchvalue) {
+  courseSection.innerHTML = "";
+  makeCourseCards("", searchvalue);
 }
 
-// search on submit in search bar
-const click_search = document.querySelector("[class = 'search-icon']")
+const click_search = document.querySelector("[class = 'search-icon']");
 click_search.addEventListener("click", (e) => {
   e.preventDefault();
   let searchvalue = document.querySelector("[class = 'search-text']").value;
-  document.getElementById("courses").scrollIntoView({behavior: "smooth", block: "start"});
-  build_search(searchvalue);
+  document
+    .getElementById("courses")
+    .scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  buildSearch(searchvalue);
 });
